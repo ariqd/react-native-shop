15de327a2969646b6ba6fefd6e682bd3f888b368
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-03-25 11:35:50 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default App;
